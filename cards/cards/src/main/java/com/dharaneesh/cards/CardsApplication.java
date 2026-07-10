c4b7fcb17d487458beaa79a57a0d7a4ea261dd79
Dharaneesh <dharaneesh@digitalcloudies.in>
2026-07-10 20:25:13 +0530

    added OpenAPIDefinition

--- cards/cards/src/main/java/com/dharaneesh/cards/CardsApplication.java
+++ cards/cards/src/main/java/com/dharaneesh/cards/CardsApplication.java
@@ -1,11 +1,36 @@
 package com.dharaneesh.cards;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "AuditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards Microservice API",
+				version = "1.0",
+				description = "API documentation for the Cards Microservice",
+				contact = @Contact(
+						name = "Dharaneesh",
+						email = "dharaneesh0203@gmail.com",
+						url = "https://github.com/dharaneesh"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.iob.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "IOBBank Cards microservice REST API Documentation",
+				url = "https://www.iob.com/swagger-ui.html"
+		)
+)
 public class CardsApplication {
 
 	public static void main(String[] args) {
